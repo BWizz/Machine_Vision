9418f6f674353a3aeee5eb4c6dedc71f319bafcc
BWizz <bjosephwisniewski@gmail.com>
2017-08-08 20:50:50 -0400

    Project 1 Initial Upload
    First project using Python's Image Lib. In this project I load an image, deconstruct it into its R,G,B arrays and create new images from the R,G,B components..

---
+++ PIL Basics/PIL_Image_Basics.py
@@ -0,0 +1,39 @@
+#Created By: BWizz
+#Date: 8/8/2017
+#Experimentation with Python Image Lib 
+from PIL import Image
+import numpy as np
+
+# Import Picture of Ellie
+myPic = Image.open("/Users/Brian/Apple_Dev/Machine Vision/Ellie.jpg")
+myPic.convert('RGB')
+# Display Image
+#myPic.show()
+
+#Split Image into RGB images
+r,g,b = myPic.split()
+
+#Load R G B into arrays
+rArray = np.array(r)
+gArray = np.array(g)
+bArray = np.array(b)
+w, h = rArray.shape
+
+
+#Create zero Array
+Zer = np.zeros((w,h,1), dtype=np.uint8)
+
+#Combined R,G, B arraus into an image class
+redImageArr = np.dstack((rArray,Zer,Zer))
+blueImageArr = np.dstack((Zer,Zer,bArray))
+greenImageArr = np.dstack((Zer,gArray,Zer))
+
+#Create images & save.
+redImage = Image.fromarray(redImageArr,'RGB')
+redImage.save("Ellie_Red.jpg")
+
+blueImage = Image.fromarray(blueImageArr,'RGB')
+blueImage.save("Ellie_Blue.jpg")
+
+greenImage = Image.fromarray(greenImageArr,'RGB')
+greenImage.save("Ellie_Green.jpg")
